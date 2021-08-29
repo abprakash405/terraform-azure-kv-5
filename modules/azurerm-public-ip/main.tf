c7ec491cf3814b2d9e6d6eb2e3b2b53e24f0acd1
abprakash405 <abprakash405@gmail.com>
2021-08-29 07:11:27 +0530

    Update main.tf

--- modules/azurerm-public-ip/main.tf
+++ modules/azurerm-public-ip/main.tf
@@ -5,4 +5,5 @@ resource "azurerm_public_ip" "example" {
   resource_group_name = var.rg_name
   location            = var.location
   allocation_method   = var.am
+  sku                 = "Standard"
 }
