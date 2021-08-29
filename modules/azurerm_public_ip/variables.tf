c76de47fc9613f2f392d87c35c6e267ea1bfd6ac
abprakash405 <abprakash405@gmail.com>
2021-08-29 06:20:59 +0530

    Create variables.tf

---
+++ modules/azurerm_public_ip/variables.tf
@@ -0,0 +1,11 @@
+variable "rg_name" {
+  description = "resource group name"
+  type        = string
+  default = ""
+}
+
+variable "location" {
+  description = "location"
+  type        = string
+  default = ""
+}
